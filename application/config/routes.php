a05c3de4922149a900c0da7321da947d05897204
epdubiso <edenpetros@rocketmail.com>
2019-08-16 19:48:33 -0700

    Adding files from gig-repo1 master branch

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 /*
 | -------------------------------------------------------------------------
 | URI ROUTING
@@ -19,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 | Please see the user guide for complete details:
 |
-|	https://codeigniter.com/user_guide/general/routing.html
+|	http://codeigniter.com/user_guide/general/routing.html
 |
 | -------------------------------------------------------------------------
 | RESERVED ROUTES
@@ -49,6 +48,67 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+/*
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+*/
+
+// $route['news/create'] = 'news/create';
+// $route['news/(:any)'] = 'news/view/$1';
+// $route['news'] = 'news';
+
+// $route['rss/(:any)'] = 'rss/$1';
+// $route['rss'] = 'rss';
+
+//admin routes
+$route['admin'] = 'admin/index';
+$route['admin/login'] = 'admin/login';
+$route['admin/logout'] = 'admin/logout';
+$route['admin/reset'] = 'admin/requestReset';
+$route['admin/resetpassword'] = 'admin/resetPassword';
+//gig routes
+$route['gig'] = 'gig/index';
+$route['gig/add'] = 'gig/add';
+$route['gig/search'] = 'gig/search';
+$route['gig/filter'] = 'gig/filter';
+$route['gig/edit'] = 'gig/edit';
+$route['gig/sendnewsletter'] = 'gig/sendnewsletter';
+$route['gig/(:any)'] = 'gig/view/$1';
+
+//venues routes
+$route['venues'] = 'venues/index';
+$route['venues/add'] = 'venues/add';
+$route['venues/edit'] = 'venues/edit';
+$route['venues/success'] = 'venues/success';
+$route['venues/(:any)'] = 'venues/view/$1';
+
+//other page routes
+$route['customer'] = 'customer';
+$route['customers'] = 'customer';
+$route['startups'] = 'startups';
+//profiles route
+$route['profile/add'] = 'profile/add';
+$route['profiles/add'] = 'profile/add';
+$route['profile/edit'] = 'profile/edit';
+$route['profiles/edit'] = 'profile/edit';
+$route['profile/changepass'] = 'profile/changePass';
+$route['profiles'] = 'profile';
+$route['profile'] = 'profile';
+$route['profile/success'] = 'profile/success';
+$route['profile/duplicat'] = 'profile/duplicat';
+
+$route['contact/view'] = 'contact/view';
+$route['contact/create'] = 'contact/create';
+$route['contact/(:any)'] = 'contact/view/$1';
+$route['contact'] = 'contact';
+$route['contact/success'] = 'contact/success';
+
+
+//bootswatch example route
+$route['example'] = 'customer/example';
+$route['profile/(:any)'] = 'profile/view/$1';
+$route['404_override'] = 'my404';
+
+//homepage routes
+$route['default_controller'] = 'welcome';
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,56 @@
 <?php
+
+/**
+ *Welcome page for Gig Central
+ *
+ * @package GIG_CENTRAL
+ * @subpackage GIG
+ * @author Kate Lee, Alexandre Daniels
+ * @version 1.0 2016/06/09
+ * @link http://newmanix.com/
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @todo none
+ */
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+/**
+ * Welcome class loads in data for the welocome_page view
+ *
+ * @todo none
+ */
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+class Welcome extends CI_Controller {
+/**
+ * constructor for the class sets active class on Home in the nav
+ *
+ * @param none
+ * @return none
+ * @todo none
+ */
+	public function __construct()
+    {//everything here is global to all methods in the controller
+        parent::__construct();
+        $this->config->set_item('nav-active', 'Home');//sets active class on Home in the nav
+    }//end constructor
+
+/**
+ * loads, but does not create, the data required to make the view page
+ *
+ * @param none
+ * @return none
+ * @todo none
+ */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->model('gig_model');
+		$data['gigs'] = $this->gig_model->getGigs();
+        $data['title'] = 'Gig Central';
+		$data['api'] = $this->config->item('googleMapsKey');
+        $data['gigTypes'] = $this->gig_model->dashboard();
+        $this->load->view($this->config->item('theme') . 'header', $data);
+		$this->load->view('welcome_page', $data);
+        $this->load->view($this->config->item('theme') . 'footer', $data);
+
 	}
 }
